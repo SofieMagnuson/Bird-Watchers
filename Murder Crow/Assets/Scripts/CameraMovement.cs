5540e39b32e945de8dbcf320c2a3b2ff67f910cd
ehagen96 <elinhage00@gmail.com>
2021-04-21 16:02:10 +0200

    work on attack

--- Murder Crow/Assets/Scripts/CameraMovement.cs
+++ Murder Crow/Assets/Scripts/CameraMovement.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public Camera cam;
     public Transform target;
     public Player player;
     public Vector3 offset, flyingOffset, noMovingOffset, targetOffset;
     public Vector2 mouseDir, smoothing, result;
     private float camSpeed, lookAroundSpeed, mouseSensitivity, drag;
-    public float tilt, maxTilt, tiltSpeed, pitch, yaw, xRot, yRot;
+    public float tilt, maxTilt, tiltSpeed, pitch, yaw, xRot, yRot, FOV, maxFOV, FOVspeed;
     public Vector3 velocity, camRot;
     public Quaternion localRot;
-    public bool isLookingAround;
+    public bool isLookingAround, attackMode;
     Vector2 rotation = new Vector2(0, 0);
 
     void Start()
     {
-        camSpeed = 0.30f;
+        camSpeed = 0.35f;
         flyingOffset = new Vector3(0.0f, 1.2f, -0.2f);
         noMovingOffset = new Vector3(0.0f, 1f, -1f);
         targetOffset = new Vector3(0.0f, 1.5f, -1f);
         offset = flyingOffset;
         velocity = Vector3.one;
         tilt = 0f;
+        FOV = 45;
+        FOVspeed = 10f;
         tiltSpeed = 10f;
         maxTilt = 20f;
+        maxFOV = 55;
         lookAroundSpeed = 100f;
         xRot = 0f;
         yRot = 0f;
@@ -37,6 +41,11 @@ public class CameraMovement : MonoBehaviour
     {
         RotateView();
 
+        if (player.reachedTarget)
+        {
+                SetAttackMode();
+            
+        }
 
         if (player.targetIsSet)
         {
@@ -52,30 +61,28 @@ public class CameraMovement : MonoBehaviour
     {
         if (!player.isGrounded)
         {
-            
+            if (!player.reachedTarget)
+            {
+                if (Input.GetKey(KeyCode.D))
+                {
+                    tilt = Mathf.Max(tilt - tiltSpeed * Time.deltaTime, -maxTilt);
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    tilt = Mathf.Min(tilt + tiltSpeed * Time.deltaTime, maxTilt);
+                } else if (tilt != 0)
+                {
+                    tilt = tilt < 0 ? Mathf.Min(tilt + tiltSpeed * 2 * Time.deltaTime, 0) : Mathf.Max(tilt - tiltSpeed * 2 * Time.deltaTime, 0);
+                }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                tilt = Mathf.Max(tilt - tiltSpeed * Time.deltaTime, -maxTilt);
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                tilt = Mathf.Min(tilt + tiltSpeed * Time.deltaTime, maxTilt);
-            } else if (tilt != 0)
-            {
-                tilt = tilt < 0 ? Mathf.Min(tilt + tiltSpeed * 2 * Time.deltaTime, 0) : Mathf.Max(tilt - tiltSpeed * 2 * Time.deltaTime, 0);
-            }
+                Vector3 targetPos = target.position + (target.rotation * offset);
+                Vector3 camPos = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, camSpeed);
+                transform.position = camPos;
 
-            Vector3 targetPos = target.position + (target.rotation * offset);
-            Vector3 camPos = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, camSpeed);
-            transform.position = camPos;
-
-            if (!Input.GetMouseButton(0))
-            {
                 camRot = new Vector3(target.eulerAngles.x + 35f, target.eulerAngles.y, tilt);
                 transform.rotation = Quaternion.Euler(camRot);
-
             }
+
 
             //mouseDir = new Vector2(Input.GetAxisRaw("Mouse X") * mouseSensitivity, Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
             //smoothing = Vector2.Lerp(smoothing, mouseDir, 1 / drag);
@@ -124,9 +131,34 @@ public class CameraMovement : MonoBehaviour
 
         //if (Input.GetMouseButton(0))
         //{
-        //    //transform.eulerAngles = new Vector3(pitch, yaw, 0f);
-               //transform.eulerAngles += (lookAroundSpeed * Time.deltaTime) * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        //    transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        //    //transform.eulerAngles += (lookAroundSpeed * Time.deltaTime) * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
         //}
     }
+
+    void SetAttackMode()
+    {
+        Vector3 startPos = transform.position;
+        //Vector3 endPos = player.targ.position + player.targ.forward * 1.2f;
+        Vector3 endPos = new Vector3(player.targ.localPosition.x + 1.2f, player.targ.localPosition.y + 1.7f, player.targ.localPosition.z - 1.2f);
+        transform.position = Vector3.Lerp(startPos, endPos, 4 * Time.deltaTime);
+        transform.LookAt(player.target);
+
+        if (startPos == endPos)
+        {
+            attackMode = true;
+        }
+
+        //Vector3 dir = player.target - transform.position;
+        //dir.y = 0f;
+        //Quaternion lookRot = Quaternion.LookRotation(dir);
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Vector3 endPos = player.targ.position + player.targ.forward * 1.2f;
+    //    Gizmos.color = Color.blue;
+    //    Gizmos.DrawCube(endPos, new Vector3(0.5f, 0.5f, 0.5f));
+    //}
 
 }
